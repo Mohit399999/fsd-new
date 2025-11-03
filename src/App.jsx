b7cd147eecb6f5acbb432fbe98540ec31c272ddc
mohit <mohitsaraswat39999@gmail.com>
2025-11-03 10:21:31 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     pic:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoFRQjM-wM_nXMA03AGDXgJK3VeX7vtD3ctA&s',
     name :'Mohit',
     roll:'2300321530119',
-    branch:'AI',
+    branch:'AI-ML',
     section:'B',
     college:'ABES'
   }
